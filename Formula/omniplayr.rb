ff2df71c9a55134cc9e9df14e523dda8a20d06a0
OmniPlayr CI <info@wokki20.nl>
2026-04-10 18:18:20 +0000

    Update omniplayr to v0.0.11

--- Formula/omniplayr.rb
+++ Formula/omniplayr.rb
@@ -1,20 +1,20 @@
 class Omniplayr < Formula
   desc "OmniPlayr CLI"
   homepage "https://github.com/OmniPlayr/OmniPlayr-CLI"
-  version "0.0.10"
+  version "0.0.11"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v0.0.10/omniplayr-macos-arm64"
+      url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v0.0.11/omniplayr-macos-arm64"
       sha256 "b6785b17a39f7b9a3606689c657b500bdc17efe5fdf4872e5a095bdc48e38e0b"
     else
-      url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v0.0.10/omniplayr-macos-x64"
+      url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v0.0.11/omniplayr-macos-x64"
       sha256 "PLACEHOLDER"
     end
   end
 
   on_linux do
-    url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v0.0.10/omniplayr-linux"
+    url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v0.0.11/omniplayr-linux"
     sha256 "bb248bde1f01f3a1e50563dfcd4b972eab7ce038d660c285125bc53404b33465"
   end
 
