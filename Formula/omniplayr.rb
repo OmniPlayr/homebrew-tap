89594dc40eccdbb1a2a8d3c7129592ee5c7a8dd9
wokki20 <105733478+levkris@users.noreply.github.com>
2026-04-28 19:41:25 +0200

    Update sha256 placeholders and install logic

--- Formula/omniplayr.rb
+++ Formula/omniplayr.rb
@@ -9,28 +9,31 @@ class Omniplayr < Formula
       sha256 "cd31f73714859030c269e8a7e1c4ab50db831da55128edac6658a64b40335dd4"
     else
       url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v1.1.0/omniplayr-macos-x64"
-      sha256 "PLACEHOLDER"
+      sha256 "REPLACE_ME"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v1.1.0/omniplayr-linux-arm64"
-      sha256 "SHA_LINUX_ARM"
+      sha256 "REPLACE_ME_ARM"
     else
       url "https://github.com/OmniPlayr/OmniPlayr-CLI/releases/download/v1.1.0/omniplayr-linux"
-      sha256 "SHA_LINUX"
+      sha256 "f777e7d97cdf700b1c011e649147ab0f682739b74dc661a7c19281bc0f43f4db"
     end
   end
 
   def install
-    binary_name = if OS.mac? && Hardware::CPU.arm?
-      "omniplayr-macos-arm64"
-    elsif OS.mac?
-      "omniplayr-macos-x64"
-    else
-      "omniplayr-linux"
-    end
+    binary_name =
+      if OS.mac? && Hardware::CPU.arm?
+        "omniplayr-macos-arm64"
+      elsif OS.mac?
+        "omniplayr-macos-x64"
+      elsif Hardware::CPU.arm?
+        "omniplayr-linux-arm64"
+      else
+        "omniplayr-linux"
+      end
 
     bin.install binary_name => "omniplayr"
   end
